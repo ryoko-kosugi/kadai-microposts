d4574fcf476729a476c22271aec1f6206338afa2
ryoko.kosugi <55273342+ryoko-kosugi@users.noreply.github.com>
2019-11-14 06:48:02 +0000

    revision favorite, unfavorite

--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -3,18 +3,17 @@
 @section('content')
 
     @if (Auth::check())
-        <div class="container">
             <div class="row">
+                <aside class="col-sm-4">
+                    @include('users.card', ['user' => $user])
+                </aside>                
                 <div class="col-sm-8">
-                    @if (Auth::id() == $user->id)
-                        @include('users.show', ['user' => Auth::user()])
-                    @endif
+                    @include('users.navtabs', ['user' => $user])
                     @if (count($microposts) > 0)
                         @include('microposts.microposts', ['microposts' => $microposts])
                     @endif
                 </div>
             </div>
-        </div>
     @endif
     
 @endsection
